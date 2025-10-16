cccb9b4fbf4e949adc8b9b94b25c4a9397c9c0ed
Christopher Van Lent <christophervanlent@Christophers-MacBook-Pro-8.local>
2025-10-15 22:48:54 -0400

    Rewrote functions to make more broadly useful

--- Project1.py
+++ Project1.py
@@ -1,5 +1,10 @@
+# Name: Christopher van Lent
+# Student ID:71095874
+# Email: cvanlent@umich.edu
 import csv
 
+# Takes in a csv file
+# Returns a dictionary of dictonaries with outer-keys of ID, inner keys of data categories, and values of data points
 def read_file(file):
 
     infile = open(file)
@@ -11,57 +16,67 @@ def read_file(file):
 
     for row in csv_reader:
 
+        "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex","year"
+
+        ID = row[0]
+        species = row[1]
         island = row[2]
-        # print(island)
-
-        year = row[-1]
-        # print(year)
-
         bill_length = row[3]
-        # print(bill_length)
+        bill_depth = row[4]
+        flipper_length = row[5]
+        body_mass = row[6]
+        sex = row[7]
+        year = row[8]
 
-        if not island in penguins:
+        d = {
 
-            d = {}
-            penguins[island] = d
+            "species": species,
+            "island": island,
+            "bill_length": bill_length,
+            "bill_depth": bill_depth,
+            "flipper_length": flipper_length,
+            "body_mass": body_mass,
+            "sex": sex,
+            "year": year
+        }
 
-        if not year in penguins[island]:
-
-            l = []
-
-            penguins[island][year] = l
-
-        penguins[island][year].append(bill_length)
+        penguins[ID] = d
 
     return penguins
 
-def calculate_average(bill_lengths):
+
+# Takes in a list of string values (as well as a few NA strings)
+# Returns average value in the list
+def calculate_average_conditional(penguins, conditional_categories, conditional_values, data_category):
     sum = 0
+    count = 0
 
-    for length in bill_lengths:
+    for penguin in penguins:
 
-        if length != "NA":
-            sum += float(length)
+        bool = True
 
-    return sum / len(bill_lengths)
+        for i in range(len(conditional_categories)):
 
-def compare_islands(penguins, year):
+            # print(conditional_categories[i])
 
-    max = 0
-    max_island = ""
+            if penguins[penguin][conditional_categories[i]] != conditional_values[i]:
 
-    for island in penguins:
+                bool = False
 
-        avg = calculate_average(penguins[island][year])
-        # print(island + ": " + str(avg))
 
-        if avg > max:
+        if bool:
 
-            max = avg
-            max_island = island
+            if penguins[penguin][data_category] != "NA":
 
-    return max_island
+                    sum += float(penguins[penguin][data_category])
+                    count += 1
 
+    return sum/count
+
+
+
+# Takes in a csv file and dictionary of dictonaries with outer-keys of islands, inner keys as years, and values as a list of bill_lengths
+# writes table of Year, Island (with highest average), and Average value into csv file for years 2007, 2008, 2009
 def write_file(penguins, file):
 
     outFile = open(file, "w")
@@ -71,16 +86,17 @@ def write_file(penguins, file):
     csv_writer.writerow(["Year", "Island", "Average"])
 
     years = ["2007", "2008", "2009"]
+    islands = ["Torgersen", "Biscoe", "Dream"]
 
     for year in years:
 
-        island = compare_islands(penguins, year)
+        for island in islands:
 
-        outlist = [year, island, calculate_average(penguins[island][year])]
+            avg = calculate_average_conditional(penguins, ["island", "year"], [island, year], "bill_length")
 
-        # print(outlist)
+            outlist = [year, island, avg]
 
-        csv_writer.writerow(outlist)
+            csv_writer.writerow(outlist)
 
     outFile.close
 
@@ -96,7 +112,13 @@ def write_file(penguins, file):
 
 penguins = read_file("penguins.csv")
 
-# average = calculate_average(data["Torgersen"]["2007"])
+# print(penguins)
+
+# average = calculate_average_conditional(penguins,["island", "year"],["Torgersen", "2009"],"bill_length")
+
+# print(average)
+
+# print(highest_average(penguins,"island",["Torgersen", "Biscoe", "Dream"], "bill_length"))
 
 
 
